d4abbecdf8567c99a284e65477aec478ba209db8
kdy1242 <dy12311229@gmail.com>
2022-04-01 09:30:52 +0900

    test commit

---
+++ c3_ex/playlist_ex/playlist/playlist.js
@@ -0,0 +1,28 @@
+/* playlist.js */
+
+window.onload = init;
+
+function init() {
+	var button = document.getElementById("addButton");
+	button.onclick = handleButtonClick;
+
+	loadPlaylist();
+}
+
+	//추가1
+function handleButtonClick(e) {
+	var textInput = document.getElementById("songTextInput");
+	var songName = textInput.value;
+	// alert("Adding" + songName);
+
+	if(songName == "") {
+		alert("곡을 입력하세요");
+	} else {
+		var li = document.createElement("li");
+		li.innerHTML = songName
+		var ul = document.getElementById("playlist");
+		ul.appendChild(li);
+		save(songName);
+	}
+}
+
